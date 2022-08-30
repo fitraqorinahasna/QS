153f2482542a4c35448e2b1a06c3d1641571cb68
fitraunyu <fitraqorinahasna@gmail.com>
2022-08-29 22:09:48 -0700

    First Commit

---
+++ novice/02-05/8.Errors and Exceptions/8.4. Raising Exceptions/index.py
@@ -0,0 +1,13 @@
+raise NameError('HiThere')
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+
+  try:
+     raise NameError('HiThere')
+ except NameError:
+     print('An exception flew by!')
+     raise
+
+An exception flew by!
+Traceback (most recent call last):
+  File "<stdin>", line 2, in <module>
